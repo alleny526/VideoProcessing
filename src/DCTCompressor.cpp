045b6aa75bfd58b5e985c2a9dab000a491102a08
Shuhan Cao <shuhan@riema.xyz>
2024-12-07 20:59:45 -0800

    feat: compress and write to file

--- src/DCTCompressor.cpp
+++ src/DCTCompressor.cpp
@@ -1,120 +1,150 @@
 # include "DCTCompressor.h"
 
-#include <vector>
 #include <cmath>
+#include <fstream>
 
-void convertIntToDouble(const int intArray[DCTCompressor::DCTSize][DCTCompressor::DCTSize], double doubleArray[DCTCompressor::DCTSize][DCTCompressor::DCTSize]) {
-    for (int i = 0; i < DCTCompressor::DCTSize; ++i) {
-        for (int j = 0; j < DCTCompressor::DCTSize; ++j) {
+void ConvertIntToDouble(const int intArray[DCTCompressor::DCT_SIZE][DCTCompressor::DCT_SIZE], double doubleArray[DCTCompressor::DCT_SIZE][DCTCompressor::DCT_SIZE]) {
+    for (int i = 0; i < DCTCompressor::DCT_SIZE; ++i) {
+        for (int j = 0; j < DCTCompressor::DCT_SIZE; ++j) {
             doubleArray[i][j] = static_cast<double>(intArray[i][j]);
         }
     }
 }
 
-void convertDoubleToInt(const double doubleArray[DCTCompressor::DCTSize][DCTCompressor::DCTSize], int intArray[DCTCompressor::DCTSize][DCTCompressor::DCTSize]) {
-    for (int i = 0; i < DCTCompressor::DCTSize; ++i) {
-        for (int j = 0; j < DCTCompressor::DCTSize; ++j) {
+void ConvertDoubleToInt(const double doubleArray[DCTCompressor::DCT_SIZE][DCTCompressor::DCT_SIZE], int intArray[DCTCompressor::DCT_SIZE][DCTCompressor::DCT_SIZE]) {
+    for (int i = 0; i < DCTCompressor::DCT_SIZE; ++i) {
+        for (int j = 0; j < DCTCompressor::DCT_SIZE; ++j) {
             intArray[i][j] = static_cast<int>(std::round(doubleArray[i][j]));
         }
     }
 }
 
-void matrixMultiply(double A[DCTCompressor::DCTSize][DCTCompressor::DCTSize], double B[DCTCompressor::DCTSize][DCTCompressor::DCTSize], double result[DCTCompressor::DCTSize][DCTCompressor::DCTSize]) {
-    for (int i = 0; i < DCTCompressor::DCTSize; ++i) {
-        for (int j = 0; j < DCTCompressor::DCTSize; ++j) {
+void MatrixMultiply(double A[DCTCompressor::DCT_SIZE][DCTCompressor::DCT_SIZE], double B[DCTCompressor::DCT_SIZE][DCTCompressor::DCT_SIZE], double result[DCTCompressor::DCT_SIZE][DCTCompressor::DCT_SIZE]) {
+    for (int i = 0; i < DCTCompressor::DCT_SIZE; ++i) {
+        for (int j = 0; j < DCTCompressor::DCT_SIZE; ++j) {
             result[i][j] = 0.0;
-            for (int k = 0; k < DCTCompressor::DCTSize; ++k) {
+            for (int k = 0; k < DCTCompressor::DCT_SIZE; ++k) {
                 result[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 }
 
-// Constructor
 void DCTCompressor() {
     // Compute the DCT matrix
-    for (int i = 0; i < DCTCompressor::DCTSize; ++i) {
-        for (int j = 0; j < DCTCompressor::DCTSize; ++j) {
+    for (int i = 0; i < DCTCompressor::DCT_SIZE; ++i) {
+        for (int j = 0; j < DCTCompressor::DCT_SIZE; ++j) {
             if (i == 0) {
-                DCTCompressor::DCTMatrix[i][j] = sqrt(1.0 / DCTCompressor::DCTSize);
+                DCTCompressor::DCT_MATRIX[i][j] = sqrt(1.0 / DCTCompressor::DCT_SIZE);
             } else {
-                DCTCompressor::DCTMatrix[i][j] = sqrt(2.0 / DCTCompressor::DCTSize) * cos(((2 * j + 1) * i * M_PI) / (2 * DCTCompressor::DCTSize));
+                DCTCompressor::DCT_MATRIX[i][j] = sqrt(2.0 / DCTCompressor::DCT_SIZE) * cos(((2 * j + 1) * i * M_PI) / (2 * DCTCompressor::DCT_SIZE));
             }
         }
     }
 
     // Transpose the DCT matrix
-    for (int i = 0; i < DCTCompressor::DCTSize; ++i) {
-        for (int j = 0; j < DCTCompressor::DCTSize; ++j) {
-            DCTCompressor::DCTMatrix_transpose[i][j] = DCTCompressor::DCTMatrix[j][i];
+    for (int i = 0; i < DCTCompressor::DCT_SIZE; ++i) {
+        for (int j = 0; j < DCTCompressor::DCT_SIZE; ++j) {
+            DCTCompressor::DCT_MATRIX_TRANSPOSE[i][j] = DCTCompressor::DCT_MATRIX[j][i];
         }
     }
 }
 
-int *DCTCompressor::DCTLocalCompressor(int blocks[DCTSize][DCTSize], int quality) {
+int *DCTCompressor::DCTLocalCompressor(int blocks[DCT_SIZE][DCT_SIZE], const int quality) {
     // Create a new int * to store the compressed data
-    double input[DCTSize][DCTSize], compressedLocalData[DCTSize][DCTSize], temp[DCTSize][DCTSize];
-    int output[DCTSize][DCTSize];
-    int result[DCTSize * DCTSize];
+    double input[DCT_SIZE][DCT_SIZE], compressed_local_data[DCT_SIZE][DCT_SIZE], temp[DCT_SIZE][DCT_SIZE];
+    int output[DCT_SIZE][DCT_SIZE];
+    int result[DCT_SIZE * DCT_SIZE];
 
     // Convert the input to double
-    convertIntToDouble(blocks, input);
+    ConvertIntToDouble(blocks, input);
 
     // perform 8*8 DCT on the blocks
-    matrixMultiply(DCTMatrix, input, temp);
-    matrixMultiply(temp, DCTMatrix_transpose, compressedLocalData);
+    MatrixMultiply(DCT_MATRIX, input, temp);
+    MatrixMultiply(temp, DCT_MATRIX_TRANSPOSE, compressed_local_data);
 
     // Convert the compressed data to int
-    convertDoubleToInt(compressedLocalData, output);
+    ConvertDoubleToInt(compressed_local_data, output);
 
     // Quantize the data
-    for (int i = 0; i < DCTSize; ++i) {
-        for (int j = 0; j < DCTSize; ++j) {
-            output[i][j] = output[i][j] >> quality;
-            result[i*DCTSize + j] = output[i][j] << quality;
+    for (int i = 0; i < DCT_SIZE; ++i) {
+        for (int j = 0; j < DCT_SIZE; ++j) {
+            result[i*DCT_SIZE+j] = output[i][j] >> quality;
         }
     }
 
     return result;
 }
 
-int (*DCTCompressor::DCTLocalDecompressor(int *compressedLocalData))[DCTCompressor::DCTSize] {
-    double temp[DCTSize][DCTSize], decompressedDoubleData[DCTSize][DCTSize];
-    int decompressedLocalData[DCTSize][DCTSize], quantizedData[DCTSize][DCTSize];
+int (*DCTCompressor::DCTLocalDecompressor(const int *compressed_local_data, const int quality))[DCTCompressor::DCT_SIZE] {
+    double temp[DCT_SIZE][DCT_SIZE], decompressed_double_data[DCT_SIZE][DCT_SIZE];
+    int decompressed_local_data[DCT_SIZE][DCT_SIZE], quantized_data[DCT_SIZE][DCT_SIZE];
 
     // Read the compressed data
-    for (int i = 0; i < DCTSize; ++i) {
-        for (int j = 0; j < DCTSize; ++j) {
-            quantizedData[i][j] = compressedLocalData[i * DCTSize + j];
+    for (int i = 0; i < DCT_SIZE; ++i) {
+        for (int j = 0; j < DCT_SIZE; ++j) {
+            quantized_data[i][j] = compressed_local_data[i * DCT_SIZE + j] << quality;
         }
     }
 
     // Convert the quantized data to double
-    convertIntToDouble(quantizedData, decompressedDoubleData);
+    ConvertIntToDouble(quantized_data, decompressed_double_data);
 
     // Perform inverse DCT
-    matrixMultiply(DCTMatrix_transpose, decompressedDoubleData, temp);
-    matrixMultiply(temp, DCTMatrix, decompressedDoubleData);
+    MatrixMultiply(DCT_MATRIX_TRANSPOSE, decompressed_double_data, temp);
+    MatrixMultiply(temp, DCT_MATRIX, decompressed_double_data);
 
     // Convert the decompressed data to int
-    convertDoubleToInt(decompressedDoubleData, decompressedLocalData);
+    ConvertDoubleToInt(decompressed_double_data, decompressed_local_data);
 
-    return decompressedLocalData;
+    return decompressed_local_data;
 }
 
-int *DCTCompressor::Compress(unsigned char *inData, int width, int height, int foreground_quality, int background_quality, std::pmr::vector<bool> &identifier) {
-    // Create a new unsigned char * to store the compressed data
-    int size = 10;
-    int *compressedData = new int[3 * width * height];
+// format of the compressed data:
+// quantization_factor coeff1 coeff2 … coeff64 coeff1 coeff2 … coeff64 coeff1 coeff2 … coeff64
+// quantization_factor coeff1 coeff2 … coeff64 coeff1 coeff2 … coeff64 coeff1 coeff2 … coeff64
+// …
+// r g b r g b r g b … r g b (The bottom blocks that cannot divided by 8)
+void DCTCompressor::Compress(const std::vector<unsigned char> &data, int foreground_quality, int background_quality, const bool *identifier) {
+    std::ofstream output_file;
+    output_file.open("compressed_data.txt", std::ios::app);
 
-    // Compress the data using DCT
-    // ...
+    int vertical_block_num = HEIGHT / DCT_SIZE;
+    int horizontal_block_num = WIDTH / DCT_SIZE;
 
-    // Save compressedData to a file
-    // ...
+    // Compress the data and write to the file
+    for (int i = 0; i < vertical_block_num; i++) {
+        for (int j = 0; j < horizontal_block_num; j++) {
+            int quality = identifier[i * horizontal_block_num + j] ? foreground_quality : background_quality;
+            output_file << (identifier[i * horizontal_block_num + j] ? foreground_quality : background_quality) << " ";
+            int base_idx = i * WIDTH * DCT_SIZE + j * DCT_SIZE;
+            for (int k = 0; k < 3; k++) {
+                int blocks[DCT_SIZE][DCT_SIZE];
+                for (int l = 0; l < DCT_SIZE; l++) {
+                    for (int m = 0; m < DCT_SIZE; m++) {
+                        blocks[l][m] = static_cast<int>(data[(base_idx + DCT_SIZE * l + m) * 3 + k]);
+                    }
+                }
+                int *compressed_data = DCTLocalCompressor(blocks, quality);
+
+                output_file << quality << " ";
+                for (int l = 0; l < DCT_SIZE * DCT_SIZE; l++) {
+                    output_file << compressed_data[l] << " ";
+                }
+            }
+            output_file << std::endl;
+        }
+    }
+
+    // Write the bottom blocks that cannot divided by 8
+    for (int i = vertical_block_num * DCT_SIZE * WIDTH * 3; i < HEIGHT * WIDTH * 3; i++) {
+        output_file << data[i] << " ";
+    }
+    output_file << std::endl;
 }
 
-unsigned char *DCTCompressor::Decompress(unsigned char *inData, int width, int height) {
+// format of rgb file: rgbrgbrgbrgb…
+unsigned char *DCTCompressor::Decompress(unsigned char *inData) {
 }
 
 
--- src/DCTCompressor.h
+++ src/DCTCompressor.h
@@ -5,35 +5,35 @@
 
 class DCTCompressor {
 public:
-    static const int DCTSize = 8;
-    static double DCTMatrix[DCTSize][DCTSize];
-    static double DCTMatrix_transpose[DCTSize][DCTSize];
+    static constexpr int WIDTH = 960;
+    static constexpr int HEIGHT = 540;
+    static constexpr int DCT_SIZE = 8;
+    static double DCT_MATRIX[DCT_SIZE][DCT_SIZE];
+    static double DCT_MATRIX_TRANSPOSE[DCT_SIZE][DCT_SIZE];
+
     /**
      * Compresses the input data using DCT.
      * @param inData The input data to compress.
-     * @param width The width of the image.
-     * @param height The height of the image.
      * @param foreground_quality The foreground quality of the compression.
      * @param background_quality The background quality of the compression.
      * @param identifier The identifier of foreground or background. 0 for background, 1 for foreground.
-     * @return The compressed data.
+     * generate the compressed data file.
      */
-    static int *Compress(unsigned char *inData, int width, int height, int foreground_quality, int background_quality, std::pmr::vector<bool> &identifier);
+    static void Compress(const std::vector<unsigned char> &data, int foreground_quality, int background_quality, const bool *identifier);
 
     /**
      * Decompresses the input data using DCT.
      * @param inData The input data to decompress.
-     * @param width The width of the image.
-     * @param height The height of the image.
-     * @return The decompressed data.
+     * @return The decompressed rgb files.
      */
-    static unsigned char *Decompress(unsigned char *inData, int width, int height);
+    unsigned char *Decompress(unsigned char *inData);
 
 private:
     DCTCompressor();
 
-    static int *DCTLocalCompressor(int blocks[DCTSize][DCTSize], int quality);
-    static int (*DCTLocalDecompressor (int *blocks))[DCTSize];
+    static int *DCTLocalCompressor(int blocks[DCT_SIZE][DCT_SIZE], int quality);
+
+    static int (*DCTLocalDecompressor (const int *compressed_local_data, int quality))[DCT_SIZE];
 };
 
 #endif //DCTCOMPRESSOR_H
